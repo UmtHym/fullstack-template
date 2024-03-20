b01929438014ac372280d931532e0469bf716da5
UmtHym <120595657+UmtHym@users.noreply.github.com>
2024-03-20 22:06:14 +0100

    first commit

---
+++ models/Post.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    require: true,
+  },
+  cloudinaryId: {
+    type: String,
+    require: true,
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+//MongoDb Collecion named here -will give lowercase plural of name. You can add a third argument to override the name that as been assigned(plural)
+module.exports = mongoose.model("Post", PostSchema);
